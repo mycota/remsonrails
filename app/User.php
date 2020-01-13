18a784488432ced35e2b347e4030f81440bfac6f
mycota <adamumh@gmail.com>
2020-01-14 01:26:37 +0530

    First commit

---
+++ app/User.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'last_name', 'email', 'phone', 'gender', 'status', 'verifyToken',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function roles()
+    {
+        return $this->beLongsToMany('App\Role');
+    }
+
+    public function hasAnyRoles($roles)
+    {
+        return null !== $this->roles()->whereIn('name', $roles)->first();
+    }
+
+    public function hasAnyRole($role)
+    {
+        return null !== $this->roles()->where('name', $role)->first();
+    }
+
+    public function customers()
+    {
+        return $this->hasMany(Customer::class);
+    }
+
+    public function logs()
+    {
+        return $this->hasMany(Logs::class);
+    }
+    
+}
