82d12a44648ca9d3a8d3018a18c3ee2f5ff97b8d
mycota <adamumh@gmail.com>
2020-02-27 13:22:25 +0530

    Created a seeder for customers and products table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CustomersTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
 
     }
 }
