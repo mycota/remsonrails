4b6c67befb7f6acfc8a8bd703e912dd066a0b929
mycota <adamumh@gmail.com>
2020-03-04 19:06:57 +0530

    Adding inputs

--- resources/views/modals/StraightLineModal.blade.php
+++ resources/views/modals/StraightLineModal.blade.php
@@ -24,12 +24,28 @@
                 
                 <div class="card-body">
 
-                        
-                    <div class="form-group row">
-                            <label for="apprft" class="col-md-4 col-form-label text-md-right">{{ __('Enter Value:') }}</label>
+                  <div class="form-group row">
+                            <label for="brck" class="col-md-4 col-form-label text-md-right">{{ __('Bracket') }}</label>
 
                             <div class="col-md-6">
-                                <input id="apprft" oninput="convert();" type="text" class="form-control" name="apprft" value="" required placeholder="Enter value here">
+                                <select id="brck" type="text" class="form-control " name="brck" required>
+                                    <option value="">Select bracket</option>
+                                    <option value="50">50</option>
+                                    <option value="75">75</option>
+                                    <option value="100">100</option>
+                                    <option value="150">Other</option>
+                                </select>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row brckshow"></div>
+
+                        <div class="form-group row">
+                            <label for="sline" class="col-md-4 col-form-label text-md-right">{{ __('Enter Value:') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="sline" oninput="straight();" type="text" class="form-control" name="sline" value="" required placeholder="Enter value here">
 
                                     <span class="invalid-feedback" role="alert">
                                         <strong></strong>
@@ -43,8 +59,8 @@
                             <label for="result" class="col-md-4 col-form-label text-md-right">{{ __('Results:') }}</label>
 
                             <div class="col-md-6">
-                                <input id="result" type="text" class="form-control" name="result" value="" readonly="">
-                                <input id="results" type="hidden" class="form-control" name="result" value="" readonly="">
+                                <input id="s_result" type="text" class="form-control" name="s_result" value="" readonly="">
+                                <input id="s_results" type="hidden" class="form-control" name="s_results" value="" readonly="">
 
                             </div>
                         </div>
